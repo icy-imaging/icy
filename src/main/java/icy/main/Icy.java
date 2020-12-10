4e9df83e6ba58716e19feda096f18e3d3471f87b
Stephane Dallongeville <stephane.dallongeville@pasteur.fr>
2020-12-10 12:22:08 +0100

    updated version

--- src/main/java/icy/main/Icy.java
+++ src/main/java/icy/main/Icy.java
@@ -108,7 +108,7 @@ public class Icy
     /**
      * Icy Version
      */
-    public static Version version = new Version("2.1.0.1");
+    public static Version version = new Version("2.1.1.0");
 
     /**
      * Main interface
