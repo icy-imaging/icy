b5e443fd94dca34fb0096acab080ba2138bb4519
danyfel80 <danyfel80@gmail.com>
2021-03-08 12:10:43 +0100

    Added selection and sorting function on descriptor selector
    Descriptor selector:
    - Added Select all checkbox
    - Now header becomes a button to sort elements on the list. Selected
    elements will move to the top and won't change their order. Unselected
    elements will be alphabetically ordered or reversed if they are already
    sorted.

--- src/main/java/icy/gui/inspector/RoiSettingPanel.java
+++ src/main/java/icy/gui/inspector/RoiSettingPanel.java
@@ -1,25 +1,21 @@
 package icy.gui.inspector;
 
-import icy.gui.component.AbstractRoisPanel.BaseColumnInfo;
-import icy.gui.component.button.IcyButton;
-import icy.preferences.XMLPreferences;
-import icy.resource.ResourceUtil;
-import icy.resource.icon.IcyIcon;
-import icy.roi.ROIDescriptor;
-import icy.roi.ROIUtil;
-
 import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
-import javax.swing.JLabel;
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
@@ -29,7 +25,16 @@ import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
 
-public class RoiSettingPanel extends JPanel implements ActionListener
+import icy.gui.component.AbstractRoisPanel.BaseColumnInfo;
+import icy.gui.component.button.IcyButton;
+import icy.gui.component.button.IcyToggleButton;
+import icy.preferences.XMLPreferences;
+import icy.resource.ResourceUtil;
+import icy.resource.icon.IcyIcon;
+import icy.roi.ROIDescriptor;
+import icy.roi.ROIUtil;
+
+public class RoiSettingPanel extends JPanel implements ActionListener, ItemListener
 {
     // GUI
     private JScrollPane scrollPaneView;
@@ -37,12 +42,14 @@ public class RoiSettingPanel extends JPanel implements ActionListener
     private JTable tableView;
     private JTable tableExport;
     private JPanel panelExportTop;
-    private JLabel lblColumnSelectionFor;
-    private JLabel lblNewLabel;
-    private IcyButton upViewBtn;
-    private IcyButton downViewBtn;
-    private IcyButton upExportBtn;
-    private IcyButton downExportBtn;
+    private JCheckBox chkHeaderSelectAllToDisplay;
+    private JCheckBox chkHeaderSelectAllToExport;
+    private IcyToggleButton btnHeaderColumnsToDisplay;
+    private IcyToggleButton btnHeaderColumnsToExport;
+    private IcyButton btnUpView;
+    private IcyButton btnDownView;
+    private IcyButton btnUpExport;
+    private IcyButton btnDownExport;
 
     // internals
     List<BaseColumnInfo> idsView;
@@ -265,10 +272,16 @@ public class RoiSettingPanel extends JPanel implements ActionListener
         column.setMaxWidth(50);
         column.setMinWidth(30);
 
-        upViewBtn.addActionListener(this);
-        downViewBtn.addActionListener(this);
-        upExportBtn.addActionListener(this);
-        downExportBtn.addActionListener(this);
+        chkHeaderSelectAllToDisplay.addItemListener(this);
+        btnHeaderColumnsToDisplay.addItemListener(this);
+        btnUpView.addActionListener(this);
+        btnDownView.addActionListener(this);
+
+        chkHeaderSelectAllToExport.addItemListener(this);
+        btnHeaderColumnsToExport.addItemListener(this);
+        btnUpExport.addActionListener(this);
+        btnDownExport.addActionListener(this);
+
     }
 
     private void initialize()
@@ -294,35 +307,45 @@ public class RoiSettingPanel extends JPanel implements ActionListener
         JPanel panelViewTop = new JPanel();
         panelView.add(panelViewTop, BorderLayout.NORTH);
         GridBagLayout gbl_panelViewTop = new GridBagLayout();
-        gbl_panelViewTop.columnWidths = new int[] {0, 0, 0, 0};
+        gbl_panelViewTop.columnWidths = new int[] {0, 0, 0, 0, 0};
         gbl_panelViewTop.rowHeights = new int[] {14, 0};
-        gbl_panelViewTop.columnWeights = new double[] {1.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_panelViewTop.columnWeights = new double[] {0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
         gbl_panelViewTop.rowWeights = new double[] {0.0, Double.MIN_VALUE};
         panelViewTop.setLayout(gbl_panelViewTop);
 
-        lblColumnSelectionFor = new JLabel("Columns to display");
-        GridBagConstraints gbc_lblColumnSelectionFor = new GridBagConstraints();
-        gbc_lblColumnSelectionFor.insets = new Insets(0, 0, 0, 5);
-        gbc_lblColumnSelectionFor.gridx = 0;
-        gbc_lblColumnSelectionFor.gridy = 0;
-        panelViewTop.add(lblColumnSelectionFor, gbc_lblColumnSelectionFor);
+        chkHeaderSelectAllToDisplay = new JCheckBox();
+        chkHeaderSelectAllToDisplay.setToolTipText("Click to select/deselect all descriptors in this list");
+        GridBagConstraints gbc_chkHeaderSelectAllToDisplay = new GridBagConstraints();
+        gbc_chkHeaderSelectAllToDisplay.insets = new Insets(0, 0, 0, 0);
+        gbc_chkHeaderSelectAllToDisplay.gridx = 0;
+        gbc_chkHeaderSelectAllToDisplay.gridy = 0;
+        panelViewTop.add(chkHeaderSelectAllToDisplay, gbc_chkHeaderSelectAllToDisplay);
 
-        upViewBtn = new IcyButton(new IcyIcon(ResourceUtil.ICON_ARROW_UP));
-        upViewBtn.setToolTipText("Change order of selected column(s)");
-        upViewBtn.setFlat(true);
-        GridBagConstraints gbc_upViewBtn = new GridBagConstraints();
-        gbc_upViewBtn.insets = new Insets(0, 0, 0, 5);
-        gbc_upViewBtn.gridx = 1;
-        gbc_upViewBtn.gridy = 0;
-        panelViewTop.add(upViewBtn, gbc_upViewBtn);
+        btnHeaderColumnsToDisplay = new IcyToggleButton("Columns to display", (IcyIcon) null);
+        btnHeaderColumnsToDisplay.setToolTipText("Click to alphabetically reorder descriptors in this list");
+        btnHeaderColumnsToDisplay.setFlat(true);
+        GridBagConstraints gbc_btnHeaderColumnsToDisplay = new GridBagConstraints();
+        gbc_btnHeaderColumnsToDisplay.insets = new Insets(0, 0, 0, 0);
+        gbc_btnHeaderColumnsToDisplay.gridx = 1;
+        gbc_btnHeaderColumnsToDisplay.gridy = 0;
+        panelViewTop.add(btnHeaderColumnsToDisplay, gbc_btnHeaderColumnsToDisplay);
 
-        downViewBtn = new IcyButton(new IcyIcon(ResourceUtil.ICON_ARROW_DOWN));
-        downViewBtn.setToolTipText("Change order of selected column(s)");
-        downViewBtn.setFlat(true);
-        GridBagConstraints gbc_downViewBtn = new GridBagConstraints();
-        gbc_downViewBtn.gridx = 2;
-        gbc_downViewBtn.gridy = 0;
-        panelViewTop.add(downViewBtn, gbc_downViewBtn);
+        btnUpView = new IcyButton(new IcyIcon(ResourceUtil.ICON_ARROW_UP));
+        btnUpView.setToolTipText("Change order of selected column(s)");
+        btnUpView.setFlat(true);
+        GridBagConstraints gbc_btnUpView = new GridBagConstraints();
+        gbc_btnUpView.insets = new Insets(0, 0, 0, 5);
+        gbc_btnUpView.gridx = 2;
+        gbc_btnUpView.gridy = 0;
+        panelViewTop.add(btnUpView, gbc_btnUpView);
+
+        btnDownView = new IcyButton(new IcyIcon(ResourceUtil.ICON_ARROW_DOWN));
+        btnDownView.setToolTipText("Change order of selected column(s)");
+        btnDownView.setFlat(true);
+        GridBagConstraints gbc_btnDownView = new GridBagConstraints();
+        gbc_btnDownView.gridx = 3;
+        gbc_btnDownView.gridy = 0;
+        panelViewTop.add(btnDownView, gbc_btnDownView);
 
         JPanel panelExport = new JPanel();
         splitPane.setRightComponent(panelExport);
@@ -339,35 +362,45 @@ public class RoiSettingPanel extends JPanel implements ActionListener
         panelExportTop = new JPanel();
         panelExport.add(panelExportTop, BorderLayout.NORTH);
         GridBagLayout gbl_panelExportTop = new GridBagLayout();
-        gbl_panelExportTop.columnWidths = new int[] {0, 0, 0, 0};
+        gbl_panelExportTop.columnWidths = new int[] {0, 0, 0, 0, 0};
         gbl_panelExportTop.rowHeights = new int[] {14, 0};
-        gbl_panelExportTop.columnWeights = new double[] {1.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_panelExportTop.columnWeights = new double[] {0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
         gbl_panelExportTop.rowWeights = new double[] {0.0, Double.MIN_VALUE};
         panelExportTop.setLayout(gbl_panelExportTop);
 
-        lblNewLabel = new JLabel("Columns to export (XLS or CSV)");
-        GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-        gbc_lblNewLabel.insets = new Insets(0, 0, 0, 5);
-        gbc_lblNewLabel.gridx = 0;
-        gbc_lblNewLabel.gridy = 0;
-        panelExportTop.add(lblNewLabel, gbc_lblNewLabel);
+        chkHeaderSelectAllToExport = new JCheckBox();
+        chkHeaderSelectAllToExport.setToolTipText("Click to select/deselect all descriptors in this list");
+        GridBagConstraints gbc_chkHeaderSelectAllToExport = new GridBagConstraints();
+        gbc_chkHeaderSelectAllToExport.insets = new Insets(0, 0, 0, 0);
+        gbc_chkHeaderSelectAllToExport.gridx = 0;
+        gbc_chkHeaderSelectAllToExport.gridy = 0;
+        panelExportTop.add(chkHeaderSelectAllToExport, gbc_chkHeaderSelectAllToExport);
 
-        upExportBtn = new IcyButton(new IcyIcon(ResourceUtil.ICON_ARROW_UP));
-        upExportBtn.setToolTipText("Change order of selected column(s)");
-        upExportBtn.setFlat(true);
-        GridBagConstraints gbc_upExportBtn = new GridBagConstraints();
-        gbc_upExportBtn.insets = new Insets(0, 0, 0, 5);
-        gbc_upExportBtn.gridx = 1;
-        gbc_upExportBtn.gridy = 0;
-        panelExportTop.add(upExportBtn, gbc_upExportBtn);
+        btnHeaderColumnsToExport = new IcyToggleButton("Columns to export (XLS or CSV)", (IcyIcon) null);
+        btnHeaderColumnsToExport.setToolTipText("Click to alphabetically reorder descriptors in this list");
+        btnHeaderColumnsToExport.setFlat(true);
+        GridBagConstraints gbc_btnHeaderColumnsToExport = new GridBagConstraints();
+        gbc_btnHeaderColumnsToExport.insets = new Insets(0, 0, 0, 0);
+        gbc_btnHeaderColumnsToExport.gridx = 1;
+        gbc_btnHeaderColumnsToExport.gridy = 0;
+        panelExportTop.add(btnHeaderColumnsToExport, gbc_btnHeaderColumnsToExport);
 
-        downExportBtn = new IcyButton(new IcyIcon(ResourceUtil.ICON_ARROW_DOWN));
-        downExportBtn.setToolTipText("Change order of selected column(s)");
-        downExportBtn.setFlat(true);
-        GridBagConstraints gbc_downExportBtn = new GridBagConstraints();
-        gbc_downExportBtn.gridx = 2;
-        gbc_downExportBtn.gridy = 0;
-        panelExportTop.add(downExportBtn, gbc_downExportBtn);
+        btnUpExport = new IcyButton(new IcyIcon(ResourceUtil.ICON_ARROW_UP));
+        btnUpExport.setToolTipText("Change order of selected column(s)");
+        btnUpExport.setFlat(true);
+        GridBagConstraints gbc_btnUpExport = new GridBagConstraints();
+        gbc_btnUpExport.insets = new Insets(0, 0, 0, 5);
+        gbc_btnUpExport.gridx = 2;
+        gbc_btnUpExport.gridy = 0;
+        panelExportTop.add(btnUpExport, gbc_btnUpExport);
+
+        btnDownExport = new IcyButton(new IcyIcon(ResourceUtil.ICON_ARROW_DOWN));
+        btnDownExport.setToolTipText("Change order of selected column(s)");
+        btnDownExport.setFlat(true);
+        GridBagConstraints gbc_btnDownExport = new GridBagConstraints();
+        gbc_btnDownExport.gridx = 3;
+        gbc_btnDownExport.gridy = 0;
+        panelExportTop.add(btnDownExport, gbc_btnDownExport);
     }
 
     void fixOrders()
@@ -444,16 +477,26 @@ public class RoiSettingPanel extends JPanel implements ActionListener
     public void actionPerformed(ActionEvent e)
     {
         final Object source = e.getSource();
+
+        if (source == btnUpView || source == btnDownView || source == btnUpExport || source == btnDownExport)
+        {
+            moveTableItem(e);
+        }
+    }
+
+    private void moveTableItem(ActionEvent e)
+    {
+        final Object source = e.getSource();
         final JTable table;
         final List<BaseColumnInfo> columnInfos;
         final int v;
 
-        if ((source == upViewBtn) || (source == downViewBtn))
+        if ((source == btnUpView) || (source == btnDownView))
         {
             table = tableView;
             columnInfos = idsView;
         }
-        else if ((source == upExportBtn) || (source == downExportBtn))
+        else if ((source == btnUpExport) || (source == btnDownExport))
         {
             table = tableExport;
             columnInfos = idsExport;
@@ -464,9 +507,9 @@ public class RoiSettingPanel extends JPanel implements ActionListener
             columnInfos = null;
         }
 
-        if ((source == upViewBtn) || (source == upExportBtn))
+        if ((source == btnUpView) || (source == btnUpExport))
             v = -1;
-        else if ((source == downViewBtn) || (source == downExportBtn))
+        else if ((source == btnDownView) || (source == btnDownExport))
             v = 1;
         else
             v = 0;
@@ -499,8 +542,171 @@ public class RoiSettingPanel extends JPanel implements ActionListener
 
             restoreSelected(table, columnInfos, selected);
             // refresh table data
-            viewModel.fireTableRowsUpdated(0, idsView.size() - 1);
-            exportModel.fireTableRowsUpdated(0, idsExport.size() - 1);
+            if (source == btnUpView || source == btnDownView)
+                viewModel.fireTableRowsUpdated(0, idsView.size() - 1);
+            else if (source == btnUpExport || source == btnDownExport)
+                exportModel.fireTableRowsUpdated(0, idsExport.size() - 1);
         }
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e)
+    {
+        Object source = e.getSource();
+        if (source == chkHeaderSelectAllToDisplay || source == chkHeaderSelectAllToExport)
+        {
+            toggleSelectAll(e);
+        }
+        else if (source == btnHeaderColumnsToDisplay || source == btnHeaderColumnsToExport)
+        {
+            sortTableElements(e);
+        }
+    }
+
+    private void toggleSelectAll(ItemEvent e)
+    {
+        final Object source = e.getSource();
+        final JTable table;
+        final List<BaseColumnInfo> columnInfos;
+        final boolean selectionValue;
+
+        if (source == chkHeaderSelectAllToDisplay)
+        {
+            table = tableView;
+            columnInfos = idsView;
+            selectionValue = chkHeaderSelectAllToDisplay.isSelected();
+        }
+        else if (source == chkHeaderSelectAllToExport)
+        {
+            table = tableExport;
+            columnInfos = idsExport;
+            selectionValue = chkHeaderSelectAllToExport.isSelected();
+        }
+        else
+        {
+            table = null;
+            columnInfos = null;
+            selectionValue = false;
+        }
+
+        if (table != null && columnInfos != null)
+        {
+            for (int j = 0; j < table.getRowCount(); j++)
+            {
+                table.setValueAt(selectionValue, j, 1);
+            }
+
+            // refresh table data
+            if (source == chkHeaderSelectAllToDisplay)
+                viewModel.fireTableRowsUpdated(0, idsView.size() - 1);
+            else if (source == chkHeaderSelectAllToExport)
+                exportModel.fireTableRowsUpdated(0, idsExport.size() - 1);
+        }
+    }
+
+    private void sortTableElements(ItemEvent e)
+    {
+        final Object source = e.getSource();
+        final JTable table;
+        final List<BaseColumnInfo> columnInfos;
+        final boolean orderIncremental;
+
+        if (source == btnHeaderColumnsToDisplay)
+        {
+            table = tableView;
+            columnInfos = idsView;
+            orderIncremental = btnHeaderColumnsToDisplay.isSelected();
+        }
+        else if (source == btnHeaderColumnsToExport)
+        {
+            table = tableExport;
+            columnInfos = idsExport;
+            orderIncremental = btnHeaderColumnsToExport.isSelected();
+        }
+        else
+        {
+            table = null;
+            columnInfos = null;
+            orderIncremental = false;
+        }
+
+        if (table != null && columnInfos != null)
+        {
+            final List<BaseColumnInfo> selected = getSelected(table, columnInfos);
+            final List<BaseColumnInfo> included = getIncluded(table, columnInfos);
+            final List<BaseColumnInfo> notIncluded = getNotIncluded(table, columnInfos);
+            included.sort(Comparator.comparingInt(info -> info.order));
+
+            for (int i = 0; i < included.size(); i++)
+            {
+                included.get(i).order = i;
+            }
+            notIncluded.sort((orderIncremental ? Comparator.comparing(i -> i.descriptor.getName().toLowerCase())
+                    : Comparator.<BaseColumnInfo, String> comparing(i -> i.descriptor.getName().toLowerCase())
+                            .reversed()));
+            for (int i = 0; i < notIncluded.size(); i++)
+            {
+                notIncluded.get(i).order = included.size() + i;
+            }
+
+            // sort lists
+            sortLists();
+
+            restoreSelected(table, columnInfos, selected);
+            if (source == btnHeaderColumnsToDisplay)
+                viewModel.fireTableRowsUpdated(0, idsView.size() - 1);
+            else if (source == btnHeaderColumnsToExport)
+                exportModel.fireTableRowsUpdated(0, idsExport.size() - 1);
+        }
+    }
+
+    private List<BaseColumnInfo> getIncluded(JTable table, List<BaseColumnInfo> columnInfos)
+    {
+        final List<BaseColumnInfo> result = new ArrayList<BaseColumnInfo>();
+        final int[] included = getIncludedIndices(table);
+
+        for (int index : included)
+            result.add(columnInfos.get(index));
+
+        return result;
+    }
+
+    private int[] getIncludedIndices(JTable table)
+    {
+        int[] included = new int[table.getRowCount()];
+        int pos = 0;
+        for (int j = 0; j < table.getRowCount(); j++)
+        {
+            if ((Boolean) table.getValueAt(j, 1))
+            {
+                included[pos++] = j;
+            }
+        }
+        return Arrays.copyOf(included, pos);
+    }
+
+    private List<BaseColumnInfo> getNotIncluded(JTable table, List<BaseColumnInfo> columnInfos)
+    {
+        final List<BaseColumnInfo> result = new ArrayList<BaseColumnInfo>();
+        final int[] notIncluded = getNotIncludedIndices(table);
+
+        for (int index : notIncluded)
+            result.add(columnInfos.get(index));
+
+        return result;
+    }
+
+    private int[] getNotIncludedIndices(JTable table)
+    {
+        int[] notIncluded = new int[table.getRowCount()];
+        int pos = 0;
+        for (int j = 0; j < table.getRowCount(); j++)
+        {
+            if (!(Boolean) table.getValueAt(j, 1))
+            {
+                notIncluded[pos++] = j;
+            }
+        }
+        return Arrays.copyOf(notIncluded, pos);
     }
 }
